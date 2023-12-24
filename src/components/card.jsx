85a5b8d4e656a554a74be1981a555187a1e5904a
Tien Dang <tien.dang@sotatek.com>
2023-12-24 15:03:44 +0700

    up

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -7,18 +7,17 @@ function Card() {
     <div>
       <div className="christmas-card">
         <input id="open" type="checkbox" checked={isOpen} />
-        <label className="open" htmlFor="open" onClick={() => setOpen(!isOpen)}></label>
+        <label
+          className="open"
+          htmlFor="open"
+          onClick={() => setOpen(!isOpen)}
+        ></label>
         <div className="card-front">
           <div className="christmas-tree"></div>
           <div className="ribbon"></div>
           <div className="text">- Click to Open -</div>
           <div className="star"></div>
           <div className="balls"></div>
-          {isOpen && (
-            <div className="image-container">
-              <div className="my-image"></div>
-            </div>
-          )}
         </div>
 
         <div className="card-inside">
@@ -26,11 +25,20 @@ function Card() {
             Merry <br /> Christmas!
           </div>
           <div className="wishes">
-            Merry Christmas người anh yêu {"<3"}. Chúc em luôn gặp những điều tốt đẹp nhất. ~Love you 
+            Merry Christmas. {"<3"}. Mãi bên nhau nhớ bé My của anh ^^ !{" "}
+            <br></br> LOVE YOU
           </div>
-          <div className="gift">
-            <div className="bow"></div>
-          </div>
+          {isOpen && (
+            <div className="images">
+              <div className="image-container">
+                <div className="my-image"></div>
+              </div>
+              <div className="heart"></div>
+              <div className="image-container">
+                <div className="my-image-2"></div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
